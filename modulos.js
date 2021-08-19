9097c76eebc853ae3d253422efda4fd323e67962
santigorbe <81199587+santigorbe@users.noreply.github.com>
2021-08-19 11:04:23 -0300

    v1

---
+++ modulos.js
@@ -0,0 +1,6 @@
+var path = require('path'); //Importarlo con el mismo nombre del modulo
+// Funciona para trabajar con url / direcciones
+
+console.log(__filename); //Muestra la direccion del archivo
+console.log(path.basename(__filename)); //Muestra el archivo que estamos usando
+
